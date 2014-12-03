01ace792c19062d57df18b1b51bb3c3936aedc42
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-03 16:01:05 +0400

    cmake find file update

---
+++ cmake/FindCSound6.cmake
@@ -0,0 +1,23 @@
+# - Try to find CSound6
+# Once done this will define
+#  CSOUND6_FOUND - System has CSound
+#  CSOUND6_INCLUDE_DIRS - The CSound include directories
+#  CSOUND6_LIBRARIES - The libraries needed to use CSound
+
+find_path(CSOUND6_INCLUDE_DIR csdl.h
+          HINTS
+            /Library/Frameworks/CsoundLib64.framework/Versions/6.0/Headers/
+            /Library/Frameworks/CsoundLib64.framework/Versions/Current/Headers/
+          PATH_SUFFIXES csound)
+
+
+#set(LIBXML2_LIBRARIES ${LIBXML2_LIBRARY} )
+set(CSOUND6_INCLUDE_DIRS ${CSOUND6_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(CSound6  DEFAULT_MSG
+                                  CSOUND6_INCLUDE_DIR)
+
+mark_as_advanced(CSOUND6_INCLUDE_DIR CSOUND6_LIBRARY)
